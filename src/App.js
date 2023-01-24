35a0a8caf0a613f466bba232d20ab59329050798
tektechnologies <info@tektechnologies.com>
2023-01-23 23:15:00 -0600

    add sample env

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import React from "react";
+import axios from 'axios';
+
+let API_KEY = process.env.REACT_APP_LOCATION_KEY;
+console.log('api key', API_KEY);
 
 class App extends React.Component {
   //constructor function 
@@ -11,22 +15,41 @@ class App extends React.Component {
   }
   
   //add helper functions 
-
-  handleSubmit = (event) =>  {
+  handleSubmit = async (event) =>  {
     event.preventDefault();
-    console.log("🚀 ~ file: App.js:17 ~ App ~ event", event);
-    
+    // // // // // // console.log("🚀 ~ file: App.js:17 ~ App ~ event", event);
+    let starWarsCharacters = await axios.get("https://swapi.dev/api/people/?page=1");
+    // // // // // // console.log("🚀 ~ file: App.js:24 ~ App ~ handleSubmit= ~ starWarsCharacters", starWarsCharacters);
+    // console.log('GGGGGGG', starWarsCharacters.data.results);
+    this.setState({
+      starWarsChars: starWarsCharacters.data.results,
+    });
   }
 
 
 
   render() {
+    // console.log('!!!!!!!',this.state.starWarsChars);
+    let starWarsList = this.state.starWarsChars.map((charName, index) => {
+      return <li key={index}>{charName.name}</li>;
+    });
+
     return <>
       <h1>Data from SWAPI!!</h1>
-
+      <ul>{starWarsList}</ul>
       <form onSubmit={this.handleSubmit}>
         <button type="submit">Display Star Wars Characters</button>
       </form>
+
+
+
+
+
+
+
+
+
+
     </>;
   }
 }
