1cd7fe6f770a17317ec3cf6f0b934d646ce31f56
tektechnologies <info@tektechnologies.com>
2023-01-23 23:29:54 -0600

    remove white space and format file

--- src/App.js
+++ src/App.js
@@ -1,51 +1,43 @@
 import React from "react";
-import axios from 'axios';
+import axios from "axios";
 
 let API_KEY = process.env.REACT_APP_LOCATION_KEY;
-console.log('api key', API_KEY);
+console.log("api key", API_KEY);
 
 class App extends React.Component {
-  //constructor function 
-  constructor(props){
+  //constructor function
+  constructor(props) {
     super(props);
-    this.state ={
+    this.state = {
       starWarsChars: [],
-    }
+    };
   }
-  
-  //add helper functions 
-  handleSubmit = async (event) =>  {
+
+  //add helper functions
+  handleSubmit = async (event) => {
     event.preventDefault();
-    let starWarsCharacters = await axios.get("https://swapi.dev/api/people/?page=1");
+    let starWarsCharacters = await axios.get(
+      "https://swapi.dev/api/people/?page=1"
+    );
     this.setState({
       starWarsChars: starWarsCharacters.data.results,
     });
-  }
-
-
+  };
 
   render() {
     let starWarsList = this.state.starWarsChars.map((charName, index) => {
       return <li key={index}>{charName.name}</li>;
     });
 
-    return <>
-      <h1>Data from SWAPI!!</h1>
-      <ul>{starWarsList}</ul>
-      <form onSubmit={this.handleSubmit}>
-        <button type="submit">Display Star Wars Characters</button>
-      </form>
-
-
-
-
-
-
-
-
-
-
-    </>;
+    return (
+      <>
+        <h1>Data from SWAPI!!</h1>
+        <ul>{starWarsList}</ul>
+        <form onSubmit={this.handleSubmit}>
+          <button type="submit">Display Star Wars Characters</button>
+        </form>
+      </>
+    );
   }
 }
 
