9fb36e9bbaa97f9f4a918c9aefd0d2f7eb3f3948
tektechnologies <info@tektechnologies.com>
2023-01-24 18:36:35 -0600

    add error handleing and try catch block

--- src/App.js
+++ src/App.js
@@ -13,36 +13,54 @@ class App extends React.Component {
       //add for city
       city: "",
       cityData: {},
+      error: false,
+      errorMessage: "",
     };
   }
 
   //add helper functions
   handleSubmit = async (event) => {
     event.preventDefault();
-
-
-    
-    let starWarsCharacters = await axios.get(
-      "https://swapi.dev/api/people/?page=1"
-    );
-    this.setState({
-      starWarsChars: starWarsCharacters.data.results,
-    });
+    try {
+      let starWarsCharacters = await axios.get(
+        "https://swapi.dev/api/people/?page=1"
+      );
+      this.setState({
+        starWarsChars: starWarsCharacters.data.results,
+        error: false,
+      });
+    } catch (error) {
+      this.setState({
+        error: true,
+        errorMessage: `An error occurred: ${error.response.status}`,
+      });
+    }
   };
 
   submitCityHandler = async (event) => {
     event.preventDefault();
-    // console.log("proof of life", event.target);
-    // https://us1.locationiq.com/v1/search?key=YOUR_ACCESS_TOKEN&q=SEARCH_STRING&format=json
-    let url = `https://us1.locationiq.com/v1/search?key=${API_KEY}&q=${this.state.city}&format=json`;
 
-    // console.log("🚀 ~ file: App.js:37 ~ App ~ submitCityHandler= ~ url", url);
-    let cityInfo = await axios.get(url);
-    console.log("🚀 ~ file: App.js:37  cityInfo", cityInfo.data[0]);
-    
-    this.setState({
-      cityData: cityInfo.data[0]
-    });
+    try {
+      // console.log("proof of life", event.target);
+      // https://us1.locationiq.com/v1/search?key=YOUR_ACCESS_TOKEN&q=SEARCH_STRING&format=json
+      let url = `https://us1.locationiq.com/v1/search?key=${API_KEY}&q=${this.state.city}&format=json`;
+
+      // console.log("🚀 ~ file: App.js:37 ~ App ~ submitCityHandler= ~ url", url);
+      let cityInfo = await axios.get(url);
+      console.log("🚀 ~ file: App.js:37  cityInfo", cityInfo.data[0]);
+
+      this.setState({
+        cityData: cityInfo.data[0],
+        error: false,
+      });
+    } catch (error) {
+      this.setState({
+        error: true,
+        errorMessage: `An error occurred: ${error.response.status}`,
+      });
+    }
+      console.log("error!!!!", this.state.error);
+      console.log("error.message!!!!", this.state.errorMessage);
   };
 
   handleCityInput = (event) => {
@@ -56,7 +74,7 @@ class App extends React.Component {
       return <li key={index}>{charName.name}</li>;
     });
     // console.log(this.state.city);
-    console.log('city',this.state.cityData);
+    console.log("city", this.state.cityData);
     return (
       <>
         <h1>Data from SWAPI!!</h1>
